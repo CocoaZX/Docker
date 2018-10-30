4b9c2d549df43e8a27194ccc39bbc1bc2c5c03d9
CocoaZX <zx349872481@gmail.com>
2018-10-30 17:35:59 +0800

    docker命令

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from  flask import Flask,send_from_directory,render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def docker_mehtod():
+    root_dir = os.path.dirname(os.getcwd())
+    return render_template("docker.html")#homepage.html在templates文件夹下
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=12333)
+
+
