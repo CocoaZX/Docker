61f1510aae6926581ee0e62ad830a63d51d03a7c
CocoaZX <zx349872481@gmail.com>
2018-11-14 21:00:44 +0800

    部署到服务器

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,14 @@
 from  flask import Flask,send_from_directory,render_template,send_file
-import os
 
 app = Flask(__name__,static_folder= "/Users/zhangxin/Desktop/Docker-/app/static")
-print( os.getcwd() + "/static")
 
-@app.route('/')
+@app.route('/docker')
 def docker_mehtod():
     return send_file("static/html/docker.html")
 
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=12333)
+    app.run(host='0.0.0.0', port=8080)
 
 
