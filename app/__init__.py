1dad5a373053ad745cd0f4546ca539e6e6d4e8e2
CocoaZX <zx349872481@gmail.com>
2018-12-02 23:35:30 +0800

    docker命令改到navi导航下

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,16 @@
 from  flask import Flask,send_from_directory,render_template,send_file
 from flask_bootstrap import Bootstrap
 
-app = Flask(__name__,static_folder= "/Users/zhangxin/Desktop/Docker-/app/static")
+app = Flask(__name__,static_folder= "/users/zhangxin/desktop/docker-/app/static")
 bootstrap = Bootstrap(app)
 
 @app.route('/docker')
 def docker_mehtod():
-    return send_file("static/html/docker.html")
+    return render_template('He.html')
 
 @app.route('/oc')
 def oc_mehtod():
-    return send_file("static/html/OC.html")
+    return send_file("static/html/oc.html")
 
 @app.route('/base')
 def base_mehtod():
@@ -19,7 +19,7 @@ def base_mehtod():
 @app.route('/')
 def navi_mehtod():
     return render_template('He.html')
-    # return send_file("static/html/He.html")
+    # return send_file("static/html/he.html")
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -29,7 +29,6 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-    app.debug = True
     app.run(host='0.0.0.0', port=8080)
 
 
